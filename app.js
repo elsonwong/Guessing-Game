369f4ca32442f515c76ea34c22089ac3ac75fd3f
elsonwong <elson.wong@live.com.sg>
2022-06-22 12:53:25 +0800

    70% complete

--- app.js
+++ app.js
@@ -4,3 +4,14 @@ while (!maximum){
 }
 const targetNum = Math.floor(Math.random() * maximum) +1;
 console.log(targetNum);
+
+let guess = parseInt (prompt ("Enter your first guess"));
+
+while (guess !== targetNum) {
+    if (guess > targetNum) {
+        guess = parseInt (prompt ("Number too high! Make another guess"))
+    } else{
+        guess = parseInt (prompt ("Your number is too low! Make another guess"))
+    }
+}
+console.log("You guessed it!")
