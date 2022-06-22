c666e1c5fb9fe363784116e5201e9e27f802d79d
elsonwong <elson.wong@live.com.sg>
2022-06-22 13:12:11 +0800

    edited attempt

--- app.js
+++ app.js
@@ -6,12 +6,14 @@ const targetNum = Math.floor(Math.random() * maximum) +1;
 console.log(targetNum);
 
 let guess = parseInt (prompt ("Enter your first guess"));
+let attempts = 1
 
 while (guess !== targetNum) {
+    attempts+=1;
     if (guess > targetNum) {
         guess = parseInt (prompt ("Number too high! Make another guess"))
     } else{
         guess = parseInt (prompt ("Your number is too low! Make another guess"))
     }
 }
-console.log("You guessed it!")
+console.log(`Correct! You have attempt ${attempts} guesses`)
